490e11462c6a03bf854db9feffa67fa53509209b
KiaShiMa <kiasar.mian@lund.ntig.se>
2016-09-15 09:38:16 +0200

    daw

--- MemoryTest/userTest/UnitTest1.cs
+++ MemoryTest/userTest/UnitTest1.cs
@@ -7,7 +7,12 @@ namespace userTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GameCoordinateShouldGetX()
+        {
+
+        }
+        [TestMethod]
+        public void GameCoordinateShouldGetY()
         {
 
         }
