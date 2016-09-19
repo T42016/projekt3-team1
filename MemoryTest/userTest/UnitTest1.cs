d6020966f72f139bdb3824b04a729976a5deb4c8
QuickT4 <pontus.quick@lund.ntig.se>
2016-09-19 09:31:22 +0200

    tests

---
+++ MemoryTest/userTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace userTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void GameCoordinateShouldGetX()
+        {
+
+        }
+        [TestMethod]
+        public void GameCoordinateShouldGetY()
+        {
+
+        }
+    }
+}
