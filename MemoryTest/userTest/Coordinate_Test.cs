dc28cf1c1875f6686359ea8575c6d6e3fcd758bb
KiaShiMa <kiasar.mian@lund.ntig.se>
2016-09-19 08:57:49 +0200

    5 added tests

---
+++ MemoryTest/userTest/Coordinate_Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MemoryLogic;
+
+namespace userTest
+{
+    [TestClass]
+    public class Coordinate_Test
+    {
+        private MemoryGame _underTest;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _underTest = new MemoryGame(4,4);
+        }
+        [TestMethod]
+        public void BoardGameShouldSetCorrectSize()
+        {
+            //Arrange & Act
+            var game = new MemoryGame(4, 4);
+
+            //Assert
+            Assert.AreEqual(game.SizeX, 4);
+            Assert.AreEqual(game.SizeY, 4);
+
+        }
+        [TestMethod]
+        public void BoardGameShouldGetX()
+        {
+            //Arrange & Act
+            var game = new MemoryGame(4, 4);
+
+            //Assert
+            Assert.AreEqual(game.SizeX, 4);
+        }
+        [TestMethod]
+        public void BoardGameShouldGetY()
+        {
+            //Arrange & Act
+            var game = new MemoryGame(4, 4);
+
+            //Assert
+            Assert.AreEqual(game.SizeY, 4);
+        }
+        [TestMethod]
+        public void BoardGameShouldGetCorrectPositionX()
+        {
+            var coord = _underTest.GetCoordinate(2, 0);
+            //Assert 
+            Assert.AreEqual(coord.X, 2);
+        }
+        [TestMethod]
+        public void BoardGameShouldGetCorrectPositionY()
+        {
+            //Arrange via setup
+            //Act
+            var coord = _underTest.GetCoordinate(0, 3);
+            //Assert 
+            Assert.AreEqual(coord.Y, 3);
+        }
+    }
+}
